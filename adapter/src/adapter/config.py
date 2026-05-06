d55479f5191e0ec2573e9008dcc458c5b9a29edb
Andrea Marson <am.dev.75@gmail.com>
2026-05-06 17:00:49 +0200

    Version 0.4.0.

--- adapter/src/adapter/config.py
+++ adapter/src/adapter/config.py
@@ -21,7 +21,7 @@ from pathlib import Path
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-VERSION = "0.3.2"
+VERSION = "0.4.0"
 
 
 class Settings(BaseSettings):
